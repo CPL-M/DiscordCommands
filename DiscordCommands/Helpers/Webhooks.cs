cfdc545f0f66e62e63562b17ef1b804d6ff53810
CPL-M <Cplmonkeyvlogs@gmail.com>
2024-06-10 21:39:45 -0400

    Fixed Async Problems

--- DiscordCommands/Helpers/Webhooks.cs
+++ DiscordCommands/Helpers/Webhooks.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 using System.Reflection;
 using Rocket.Core.Logging;
 using static UnityEngine.GraphicsBuffer;
+using ShimmyMySherbet.DiscordWebhooks.Models.Exceptions;
 
 namespace DiscordCommands.Helpers
 {
@@ -18,54 +19,48 @@ namespace DiscordCommands.Helpers
     {
         public static void SuggestForum(UnturnedPlayer caller, string suggestion, string title)
         {
-            ThreadPool.QueueUserWorkItem((WaitCallback)(async _ =>
+            try
             {
-                try
-                {
-                    var channel = new DiscordWebhookChannel(Main.Instance.Configuration.Instance.Suggest.Webhook);
+                var channel = new DiscordWebhookChannel(Main.Instance.Configuration.Instance.Suggest.Webhook);
 
-                    var message = new WebhookMessage()
-                    .CreateThread(title)
-                    .PassEmbed()
-                    .WithTitle(title)
-                    .WithColor(EmbedColor.LightGoldenrodYellow)
-                    .WithAuthor($"Suggestor: {caller.CharacterName}", "https://steamcommunity.com/profiles/" + caller.CSteamID.ToString())
-                    .WithDescription(suggestion)
-                    .WithFooter("[DiscordCommands] " + DateTime.Now.ToString("dddd, dd MMMM, yyyy"))
-                    .Finalize();
+                var message = new WebhookMessage()
+                .CreateThread(title)
+                .PassEmbed()
+                .WithTitle(title)
+                .WithColor(EmbedColor.LightGoldenrodYellow)
+                .WithAuthor($"Suggestor: {caller.CharacterName}", "https://steamcommunity.com/profiles/" + caller.CSteamID.ToString())
+                .WithDescription(suggestion)
+                .WithFooter("[DiscordCommands] " + DateTime.Now.ToString("dddd, dd MMMM, yyyy"))
+                .Finalize();
 
-                    var sent = await channel.PostMessageAsync(message);
-                }
-                catch (Exception ex)
-                {
-                    Logger.LogException(ex, "Exception occurred in Suggest method");
-                }
-            }));
+                var sent = channel.PostMessage(message);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex, "Exception occurred in Suggest method");
+            }
         }
         public static void Suggest(UnturnedPlayer caller, string suggestion, string title)
         {
-            ThreadPool.QueueUserWorkItem((WaitCallback)(async _ =>
+            try
             {
-                try
-                {
-                    var channel = new DiscordWebhookChannel(Main.Instance.Configuration.Instance.Suggest.Webhook);
+                var channel = new DiscordWebhookChannel(Main.Instance.Configuration.Instance.Suggest.Webhook);
 
-                    var message = new WebhookMessage()
-                    .PassEmbed()
-                    .WithTitle("Suggestion")
-                    .WithColor(EmbedColor.LightGoldenrodYellow)
-                    .WithField(title, suggestion)
-                    .WithField("From", $"[{caller.CharacterName}](https://steamcommunity.com/profiles/{caller.CSteamID})", false)
-                    .WithFooter("[DiscordCommands] " + DateTime.Now.ToString("dddd, dd MMMM, yyyy"))
-                    .Finalize();
+                var message = new WebhookMessage()
+                .PassEmbed()
+                .WithTitle("Suggestion")
+                .WithColor(EmbedColor.LightGoldenrodYellow)
+                .WithField(title, suggestion)
+                .WithField("From", $"[{caller.CharacterName}](https://steamcommunity.com/profiles/{caller.CSteamID})", false)
+                .WithFooter("[DiscordCommands] " + DateTime.Now.ToString("dddd, dd MMMM, yyyy"))
+                .Finalize();
 
-                    var sent = await channel.PostMessageAsync(message);
-                }
-                catch (Exception ex)
-                {
-                    Logger.LogException(ex, "Exception occurred in Suggest method");
-                }
-            }));
+                var sent = channel.PostMessage(message);
+            }
+            catch (RatelimitedException ex)
+            {
+                Logger.LogException(ex, "Exception occurred in Suggest method");
+            }
         }
         public static void Commend(UnturnedPlayer caller, UnturnedPlayer target, string reason)
         {
@@ -87,7 +82,7 @@ namespace DiscordCommands.Helpers
 
                     var sent = await channel.PostMessageAsync(message);
                 }
-                catch (Exception ex)
+                catch (RatelimitedException ex)
                 {
                     Logger.LogException(ex, "Exception occurred in Suggest method");
                 }
@@ -95,29 +90,26 @@ namespace DiscordCommands.Helpers
         }
         public static void Praise(UnturnedPlayer caller, UnturnedPlayer target, string reason)
         {
-            ThreadPool.QueueUserWorkItem((WaitCallback)(async _ =>
+            try
             {
-                try
-                {
-                    var channel = new DiscordWebhookChannel(Main.Instance.Configuration.Instance.Praise.Webhook);
+                var channel = new DiscordWebhookChannel(Main.Instance.Configuration.Instance.Praise.Webhook);
 
-                    var message = new WebhookMessage()
-                    .PassEmbed()
-                    .WithTitle("Staff Praise")
-                    .WithColor(EmbedColor.MediumSpringGreen)
-                    .WithField("Staff Member", $"[{target.CharacterName}](https://steamcommunity.com/profiles/{target.CSteamID})", true)
-                    .WithField("Reason", reason, false)
-                    .WithField("From", $"[{caller.CharacterName}](https://steamcommunity.com/profiles/{caller.CSteamID})", false)
-                    .WithFooter("[DiscordCommands] " + DateTime.Now.ToString("dddd, dd MMMM, yyyy"))
-                    .Finalize();
+                var message = new WebhookMessage()
+                .PassEmbed()
+                .WithTitle("Staff Praise")
+                .WithColor(EmbedColor.MediumSpringGreen)
+                .WithField("Staff Member", $"[{target.CharacterName}](https://steamcommunity.com/profiles/{target.CSteamID})", true)
+                .WithField("Reason", reason, false)
+                .WithField("From", $"[{caller.CharacterName}](https://steamcommunity.com/profiles/{caller.CSteamID})", false)
+                .WithFooter("[DiscordCommands] " + DateTime.Now.ToString("dddd, dd MMMM, yyyy"))
+                .Finalize();
 
-                    var sent = await channel.PostMessageAsync(message);
-                }
-                catch (Exception ex)
-                {
-                    Logger.LogException(ex, "Exception occurred in Suggest method");
-                }
-            }));
+                var sent = channel.PostMessage(message);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex, "Exception occurred in Suggest method");
+            }
         }
     }
 }
